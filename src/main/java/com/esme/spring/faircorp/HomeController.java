7fffbf603270b1e177cbbc8dbde9e571dd1f3423
morad abouharb <morad.abou.harb@gmail.com>
2018-11-05 02:51:13 +0100

    add sql query to the home controller

--- src/main/java/com/esme/spring/faircorp/HomeController.java
+++ src/main/java/com/esme/spring/faircorp/HomeController.java
@@ -5,8 +5,12 @@ import com.esme.spring.faircorp.Model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/Building")
@@ -14,8 +18,12 @@ public class HomeController {
 
     @Autowired
     private LightDao L1;
+
     @Autowired
     private RoomDao R1;
+
+    @PersistenceContext
+    private EntityManager em;
 
     public HomeController(){
     }
@@ -32,6 +40,21 @@ public class HomeController {
         Optional<Light> light = L1.findById(id);
         return light.get();
     }
+/*
+    //http://localhost:8080/api/Building/Lightss?level=2
+    @RequestMapping(value = "Lightss",method = RequestMethod.GET,produces = "application/json")
+    public List<Light> GetLightON(@RequestParam("Level") int Level){
+        String Qsql = "SELECT c FROM Light c WHERE c.Level =:Level";
+        return em.createQuery(Qsql,Light.class).setParameter("Level",Level).getResultList();
+    }
+*/
+    //http://localhost:8080/api/Building/Lightss?Status=on
+    @RequestMapping(value = "Lightss",method = RequestMethod.GET,produces = "application/json")
+    public List<Light> GetLightON(@RequestParam("Status") String Status){
+        String Qsql = "SELECT c FROM Light c WHERE LOWER(c.Status) =:Status";
+        return em.createQuery(Qsql,Light.class).setParameter("Status",Status).getResultList();
+    }
+
 
     //http://localhost:8080/api/Building/Lights
     @RequestMapping(value = "Lights",method = RequestMethod.GET, produces = "application/json")
@@ -45,5 +68,5 @@ public class HomeController {
         return R1.findAll();
     }
 
+
 }
-    
--- src/main/java/com/esme/spring/faircorp/Model/Room.java
+++ src/main/java/com/esme/spring/faircorp/Model/Room.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
+@Table(name = "ROOM")
 public class Room {
 
     @Id
